39987d5430914e23af7075f0e85315e19080738c
Shinichi Wijaya <wijayashinichi@gmail.com>
2025-09-24 23:58:43 +0700

    feat: use shared preferences to make cart item persists

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -4,13 +4,20 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:provider_shopper/models/catalog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CartModel extends ChangeNotifier {
+  static const _cartItemsKey = '';
+
   /// The private field backing [catalog].
   late CatalogModel _catalog;
 
   /// Internal, private state of the cart. Stores the ids of each item.
   final List<int> _itemIds = [];
+
+  CartModel() {
+    _loadCart();
+  }
 
   /// The current catalog. Used to construct items from numeric ids.
   CatalogModel get catalog => _catalog;
@@ -34,6 +41,7 @@ class CartModel extends ChangeNotifier {
   /// Adds [item] to cart. This is the only way to modify the cart from outside.
   void add(Item item) {
     _itemIds.add(item.id);
+    _saveCart();
     // This line tells [Model] that it should rebuild the widgets that
     // depend on it.
     notifyListeners();
@@ -41,8 +49,24 @@ class CartModel extends ChangeNotifier {
 
   void remove(Item item) {
     _itemIds.remove(item.id);
+    _saveCart();
     // Don't forget to tell dependent widgets to rebuild _every time_
     // you change the model.
     notifyListeners();
+  }
+
+  Future<void> _loadCart() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cartItems = prefs.getStringList('cart_items');
+    if (cartItems != null) {
+      _itemIds.addAll(cartItems.map((id) => int.parse(id)));
+    }
+    notifyListeners();
+  }
+
+  Future<void> _saveCart() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cartItems = _itemIds.map((id) => id.toString()).toList();
+    await prefs.setStringList('cart_items', cartItems);
   }
 }
